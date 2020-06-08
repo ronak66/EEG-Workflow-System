927d6858ba09357fd23fa11843c7fbc842938a94
ronak66 <ronakdoshi36@gmail.com>
2020-06-08 08:19:00 +0530

    Error: cloudflare import error fix

--- blocks/test1/__init__.py
+++ blocks/test1/__init__.py
@@ -2,6 +2,6 @@ from blocks.test1.AddSub.Addition import Addition
 from blocks.test1.AddSub.Subraction import Subraction
 
 string_classobject_mapping = {
-    "Addition": Addition(),
-    "Subraction": Subraction()
+    Addition.name: Addition(),
+    Subraction.name: Subraction()
 }
